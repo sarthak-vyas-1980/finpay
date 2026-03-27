bec97b71e264eb89007de57fcc0f54561904f41a
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-03-27 20:10:12 +0530

    fix prisma engine bundling

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["@prisma/client"]
-  }
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+  webpack: (config) => {
+    config.externals.push("@prisma/client");
+    return config;
+  },
 };
 
 module.exports = nextConfig;
