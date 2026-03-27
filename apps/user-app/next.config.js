c316fb015a502718a11d9fb20b7503cbfee42ea8
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-03-27 14:51:08 +0530

    fix: monorepo db resolution for vercel

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,7 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     externalDir: true,
   },
+  transpilePackages: ["@repo/db", "@repo/ui", "@repo/store"],
 };
 
 module.exports = nextConfig;
