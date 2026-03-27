4f131deb8bdf7ff1be2260bee34f7e72fec54747
Sarthak Vyas <sarthakvyas41@gmail.com>
2026-03-27 15:05:23 +0530

    fix: prisma engine for vercel

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
     externalDir: true,
   },
   transpilePackages: ["@repo/db", "@repo/ui", "@repo/store"],
+
+  webpack: (config) => {
+    config.externals = config.externals || [];
+    return config;
+  },
 };
 
 module.exports = nextConfig;
